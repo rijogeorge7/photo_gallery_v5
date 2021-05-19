f0bd5c231d17e913b34cc0b98bd02b947599b5e9
Rijo George <rijogeorge@rijos-imac.lan>
2021-05-18 17:56:26 -0700

    Write your own JavaScript code instead of using the included plugin for the search/content filtering requirement.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
 baguetteBox.run('.gallery');
+
+function filterSearch() {
+    let input = document.getElementById('search').value
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('gallery-item');
+    for (i = 0; i < x.length; i++) { 
+        let caption = x[i].getAttribute("data-caption");
+        if (!caption.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+        }
+        else {
+            x[i].style.display="inline";                 
+        }
+    }
+}
+
+document.getElementById("search").addEventListener("keyup", filterSearch);
